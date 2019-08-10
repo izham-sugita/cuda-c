3a6a0fc4902b198a23171b1f74cfcf2bf1e1c178
sugita <sugita5019@gmail.com>
2019-08-10 15:40:38 +0800

    first commit

---
+++ cuda-vector-add.cu
@@ -0,0 +1,45 @@
+/*
+The reference homepage
+https://cuda-tutorial.readthedocs.io/en/latest/tutorials/tutorial01/
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 1000000
+
+void vector_add(float *out, float *a, float *b, int n)
+{
+  for(int i=0; i<n; ++i){
+    out[i] = a[i] + b[i];
+  }
+}
+
+/*cuda gpu kernel*/
+/*naive kernel*/
+__global__ void gpu_vector_add(float *a, float *b,
+			       float *c, int n)
+{
+  for(int i=0; i<n; ++i){
+    c[i] = a[i] + b[i];
+  }
+}
+
+int main()
+{
+  float *a, *b, *out;
+
+  //Allocate memory
+  a = (float*)malloc(sizeof(float)*N);
+  b = (float*)malloc(sizeof(float)*N);
+  out = (float*)malloc(sizeof(float)*N);
+
+  //Initialize
+  for(int i=0; i<N; ++i){
+    a[i] = 1.0f; b[i]=1.0f; out[i]=0.0f;
+  }
+
+  //call function
+  vector_add(out, a,b, N);
+  
+}
